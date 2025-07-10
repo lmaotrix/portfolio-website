8d1c02b9dd92664113dfa30efd1f4b606534c3cd
lmaotrix <coco.gian07@gmail.com>
2025-07-10 10:01:01 +0200

    initial commit

---
+++ src/main.js
@@ -0,0 +1,63 @@
+let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+    navbar.classList.toggle('open-menu')
+    menu.classList.toggle("move");
+}
+window.onscroll = () => {
+    navbar.classList.remove('open-menu')
+    menu.classList.remove("move");
+}
+
+// Email JS
+
+function validate() {
+    let name = document.querySelector(".name");
+    let email = document.querySelector(".email");
+    let msg = document.querySelector(".message");
+    let sendBtn = document.querySelector(".send-btn");
+
+    sendBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (name.value == "" || email.value == "" || msg.value == "") {
+            emptyerror();
+        } else {
+            sendmail (name.value, email.value, msg.value);
+            success();
+        }
+    })
+}
+validate();
+
+function sendmail(name,email,msg) {
+    emailjs.send("service_wazwk46","template_i7lodfj",{
+        from_name: email,
+        to_name: name,
+        message: msg,
+        });
+}
+
+function emptyerror() {
+    swal({
+        title: "There's something wrong!",
+        text: "Fields cannot be empty!",
+        icon: "error",
+      });
+}
+
+function success() {
+    swal({
+        title: "Email sent!",
+        text: "I'll reply as soon as possible!",
+        icon: "success",
+      });
+}
+
+// Header background change on scroll
+
+let header = document.querySelector('header')
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('header-active', window.scrollY > 0);
+});
